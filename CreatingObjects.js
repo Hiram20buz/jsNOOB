18824a2a24728358c396108596b982829bc4e53b
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-06-25 10:32:13 -0700

    Create CreatingObjects.js

---
+++ CreatingObjects.js
@@ -0,0 +1,12 @@
+function person(name, age, color) {
+    this.name = name;
+    this.age = age;
+    this.favColor = color;
+}
+
+var p1 = new person("John", 42, "green");
+var p2 = new person("Amy", 21, "red");
+
+
+console.log(p1.age);
+console.log(p2.name);
